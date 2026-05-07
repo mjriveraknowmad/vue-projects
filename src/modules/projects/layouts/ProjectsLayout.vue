e718b209ef8b7cc395a0a0028635623e5b28cedb
Marcos Rivera <mjrivera@knowmadmood.com>
2026-05-07 10:56:08 +0200

    feat: add TopMenu and SideMenu components to ProjectsLayout with DaisyUi

--- src/modules/projects/layouts/ProjectsLayout.vue
+++ src/modules/projects/layouts/ProjectsLayout.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="flex flex-col">
+    <!-- Top Menu -->
+    <top-menu />
+
     <main class="flex flex-row">
+      <!-- Side Menu -->
+      <side-menu />
+
       <!-- RouterView -->
       <router-view />
     </main>
@@ -8,4 +14,6 @@
 </template>
 
 <script lang="ts" setup>
+import TopMenu from '@/modules/common/components/TopMenu.vue';
+import SideMenu from '../components/SideMenu.vue';
 </script>
