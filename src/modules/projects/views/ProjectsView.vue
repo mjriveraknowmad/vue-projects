73174c5539a6adf3b69309cc3f88f63dbe8915c7
Marcos Rivera <mjrivera@knowmadmood.com>
2026-05-07 11:18:37 +0200

    feat: add CustomModal component with slots and integrate into ProjectsView

--- src/modules/projects/views/ProjectsView.vue
+++ src/modules/projects/views/ProjectsView.vue
@@ -21,13 +21,41 @@
     </table>
   </div>
 
+  <custom-modal :open="customModalOpen">
+    <template #header>
+      <h1 class="text-3xl">Titulo del modal</h1>
+    </template>
+
+    <template #body>
+      <p>
+        Nulla consequat non ullamco mollit est quis duis pariatur cupidatat consequat Lorem cillum.
+      </p>
+    </template>
+
+    <template #footer>
+      <div class="flex justify-end">
+        <button @click="customModalOpen = false" class="btn mr-4">Close</button>
+        <button @click="customModalOpen = false" class="btn btn-primary">Aceptar</button>
+      </div>
+    </template>
+  </custom-modal>
+
   <fab-button>
     <AddCircle />
   </fab-button>
 
+  <fab-button @click="customModalOpen = true" position="bottom-left">
+    <ModalIcon />
+  </fab-button>
 </template>
 
 <script lang="ts" setup>
+import CustomModal from '@/modules/common/components/CustomModal.vue';
 import FabButton from '@/modules/common/components/FabButton.vue';
 import AddCircle from '@/modules/common/icons/AddCircle.vue';
+import ModalIcon from '@/modules/common/icons/ModalIcon.vue';
+import { ref } from 'vue';
+
+const customModalOpen = ref(false);
+
 </script>
